9bbc4d80215f8b9965a1fe5ae3ae5a46a149b315
Henriette Wiuff <enni_wiuff@hotmail.com>
2019-11-01 11:53:03 +0100

    opret js

---
+++ javascript.js
@@ -0,0 +1,9 @@
+//Til burger menu
+function myFunction(x) {
+  x.classList.toggle("change");
+    openNav();
+}
+
+function openNav() {
+  document.getElementById("mySidenav").classList.toggle("active");
+}
