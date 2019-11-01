c45bf33002aef4e0de9e2f890975a5fdd9bef1ad
Signe Kræmmer <signekraemmer@hotmail.com>
2019-11-01 13:05:16 +0100

    FAQ er opdateret og der mangler kun at linke det sammen.

--- javascript.js
+++ javascript.js
@@ -7,3 +7,20 @@ function myFunction(x) {
 function openNav() {
   document.getElementById("mySidenav").classList.toggle("active");
 }
+
+
+// FAQ / HELP SECTION
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
