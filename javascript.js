6a935647e0172a8281998fae725eb2957c67ff73
Signe Kræmmer <signekraemmer@hotmail.com>
2019-11-04 16:48:29 +0100

    Supportsiden er opdateret, men sat på pause. Profil siden er påbegyndt.

--- javascript.js
+++ javascript.js
@@ -8,7 +8,7 @@ function openNav() {
   document.getElementById("mySidenav").classList.toggle("active");
 }
 
-
+// THOMAS HELP
 // FAQ / HELP SECTION
 var coll = document.getElementsByClassName("collapsible");
 var i;
@@ -16,12 +16,18 @@ var i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
+
     var content = this.nextElementSibling;
+
     if (content.style.display === "block") {
       content.style.display = "none";
-    } else {
-      content.style.display = "block";
     }
+    else {
+      content.style.display = "block";
+
+    }
+  //  var turn = document.getElementsByClassName("arrowfaq");
+      //turn[i].classList.toggle("arrowturn");
   });
 }
 
